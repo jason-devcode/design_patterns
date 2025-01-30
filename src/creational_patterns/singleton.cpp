a6ae7ad11a608e0ab363f91d1f3ab60438cbb671
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-01-29 23:53:06 -0500

    refactor: remove redundant scope resolution operator and added some comments

--- src/creational_patterns/singleton.cpp
+++ src/creational_patterns/singleton.cpp
@@ -7,20 +7,23 @@ class MySingleton {
     MySingleton()=default;
     ~MySingleton()=default;
     
+    // method to instantiate(if is not already instantiate) and get singleton instance
     static std::shared_ptr<MySingleton> getInstance() {
-      if( !MySingleton::instance ) {
-        MySingleton::instance = std::make_shared<MySingleton>();
+      if( !instance ) {
+        instance = std::make_shared<MySingleton>();
       }
-      return MySingleton::instance;
+      return instance;
     }
-
+    
+    // a test method to test singleton instance
     void greeting() {
       std::cout << "Hello, Singleton!" << std::endl;
     }
   private:
     static std::shared_ptr<MySingleton> instance;
 };
-std::shared_ptr<MySingleton> MySingleton::instance;
+// initialize shared pointer member to singleton instance
+std::shared_ptr<MySingleton> MySingleton::instance = nullptr;
 
 
 int main() {
