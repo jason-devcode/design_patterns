ec52f43529685a309a61f27bd48feceae960103a
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-02-05 22:29:07 -0500

    feat: added sample adapter pattern

--- src/structural_patterns/adapter.cpp
+++ src/structural_patterns/adapter.cpp
@@ -1,23 +1,65 @@
 #include <iostream>
+#include <memory>
+#include <stdexcept>
 
-class File {};
-
-class XMLFile : public File {
-    public:
-        ~XMLFile()=default;
-
+// Base class for File
+class File
+{
+public:
+    virtual ~File() = default;
 };
 
-class JSONFile : public File {
-    public:
-        ~JSONFile()=default;
+// Derived class for XMLFile
+class XMLFile : public File
+{
+public:
+    ~XMLFile() = default;
 
+    void display() const
+    {
+        std::cout << "This is an XML file." << std::endl;
+    }
 };
 
+// Derived class for JSONFile
+class JSONFile : public File
+{
+public:
+    ~JSONFile() = default;
 
+    void display() const
+    {
+        std::cout << "This is a JSON file." << std::endl;
+    }
+};
 
-int main() {
+// Adapter class to convert XML to JSON
+class XMLtoJSONAdapter
+{
+public:
+    ~XMLtoJSONAdapter() = default;
 
+    std::unique_ptr<JSONFile> convert(std::unique_ptr<XMLFile> xmlFile)
+    {
+        // Convert XMLFile to JSONFile
+        std::cout << "Converting XML to JSON..." << std::endl;
+        return std::make_unique<JSONFile>();
+    }
+};
+
+int main()
+{
+    // Create an XML file
+    std::unique_ptr<XMLFile> xmlFile = std::make_unique<XMLFile>();
+
+    // Create an adapter and convert the XML file to JSON
+    XMLtoJSONAdapter adapter;
+
+    // convert xml file to json file
+    std::unique_ptr<JSONFile> jsonFile = adapter.convert(std::move(xmlFile));
+
+    // Display the content of the JSON file
+    jsonFile->display();
 
     return 0;
 }
