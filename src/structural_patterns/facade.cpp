4b8d270d7736bae73860096276bb386ee1970dfe
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-02-13 23:32:22 -0500

    feat: add facade pattern

--- src/structural_patterns/facade.cpp
+++ src/structural_patterns/facade.cpp
@@ -1,12 +1,48 @@
-#include <memory>
 #include <iostream>
-#include <stdexcept>
 
+class FlightBookingSystem
+{
+public:
+    static void book_flight()
+    {
+        std::cout << "Booking a flight ticket..." << std::endl;
+        std::cout << "Successful flight ticket booked!" << std::endl;
+    }
+};
+class HotelBookingSystem
+{
+public:
+    static void book_hotel()
+    {
+        std::cout << "Booking a hotel room..." << std::endl;
+        std::cout << "Successful hotel room booked!" << std::endl;
+    }
+};
+class CarRentalSystem
+{
+public:
+    static void rent_car()
+    {
+        std::cout << "Renting Car..." << std::endl;
+        std::cout << "Successful car rented!" << std::endl;
+    }
+};
 
+class TravelFacade
+{
+public:
+    ~TravelFacade() = default;
 
-int main() {
+    static void book_trip()
+    {
+        FlightBookingSystem::book_flight();
+        HotelBookingSystem::book_hotel();
+        CarRentalSystem::rent_car();
+    }
+};
 
-
+int main()
+{
+    TravelFacade::book_trip();
     return 0;
 }
-
