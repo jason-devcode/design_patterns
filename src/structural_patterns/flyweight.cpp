5f73fe3f807dc9d65e410980e62395ef300d4936
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-02-17 23:42:06 -0500

    feat: complete flyweight pattern

--- src/structural_patterns/flyweight.cpp
+++ src/structural_patterns/flyweight.cpp
@@ -4,61 +4,73 @@
 #include <map>
 #include <string>
 
-// Simulates a texture data
 class Texture
 {
 public:
-    Texture() = default;
-    Texture(int width, int height, std::string texture_name) : m_width(width), m_height(height), m_texture_name(texture_name) {}
-    ~Texture() = default;
-
-    int get_width() const
-    {
-        return m_width;
-    }
-    int get_height() const
-    {
-        return m_height;
-    }
-    std::string get_name() const
-    {
-        return m_texture_name;
-    }
+    Texture(int width, int height, std::string texture_name) 
+        : m_width(width), m_height(height), m_texture_name(texture_name) {}
 
     void print_texture_info() const
     {
         std::cout << std::string(40, '-') << std::endl;
-        std::cout << "Texture name: " << this->get_name() << std::endl;
-        std::cout << "Dimensions: " << this->get_width() << "x" << this->get_height() << " pixels" << std::endl;
+        std::cout << "Texture name: " << m_texture_name << std::endl;
+        std::cout << "Dimensions: " << m_width << "x" << m_height << " pixels" << std::endl;
         std::cout << std::string(40, '-') << std::endl;
     }
 
-    friend std::ostream &operator<<(std::ostream &out, const Texture &tex)
-    {
-        tex.print_texture_info();
-        return out;
-    }
-
-protected:
+private:
     int m_width;
     int m_height;
     std::string m_texture_name;
 };
 
-using TextureDictionary = std::map<int, Texture>;
-
-// Model Mesh simulate
-class Mesh3D
+class TextureFactory
 {
 public:
-protected:
+    static std::shared_ptr<Texture> get_texture(const std::string &name, int width, int height)
+    {
+        std::string key = name + "_" + std::to_string(width) + "x" + std::to_string(height);
+
+        if (textures.find(key) == textures.end())
+        {
+            textures[key] = std::make_shared<Texture>(width, height, name);
+        }
+
+        return textures[key];
+    }
+
+private:
+    static std::map<std::string, std::shared_ptr<Texture>> textures;
+};
+
+std::map<std::string, std::shared_ptr<Texture>> TextureFactory::textures;
+
+class GameObject
+{
+public:
+    GameObject(const std::string &name, const std::string &texture_name, int width, int height)
+        : m_name(name), m_texture(TextureFactory::get_texture(texture_name, width, height)) {}
+
+    void draw() const
+    {
+        std::cout << "Drawing object: " << m_name << std::endl;
+        m_texture->print_texture_info();
+    }
+
+private:
+    std::string m_name;
+    std::shared_ptr<Texture> m_texture; // Flyweight
 };
 
 int main()
 {
-    Texture tex(512, 512, "bricks");
+    GameObject tree1("Tree_1", "TreeTexture", 256, 256);
+    GameObject tree2("Tree_2", "TreeTexture", 256, 256);
+    GameObject rock("Rock_1", "RockTexture", 128, 128);
 
-    std::cout << tex;
+    tree1.draw();
+    tree2.draw();
+    rock.draw();
 
     return 0;
 }
