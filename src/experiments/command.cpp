59925bfd48f2aef0604f34eb7ffd56d1fcd3a71a
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-02-24 22:40:04 -0500

    feat: culminate command pattern experiment

--- src/experiments/command.cpp
+++ src/experiments/command.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <string>
 #include <stdexcept>
 
 class Command
@@ -12,11 +13,14 @@ class Invoker
 {
 public:
     using SharedCommand = std::shared_ptr<Command>;
-
+    Invoker() = default;
+    Invoker(SharedCommand command) : m_command(command) {}
     ~Invoker() = default;
+
     void executeCommand()
     {
-        m_command->execute();
+        if (m_command != nullptr)
+            m_command->execute();
     }
 
     void setCommand(SharedCommand command)
@@ -31,12 +35,54 @@ private:
 class Button
 {
 public:
+    using SharedInvoker = std::shared_ptr<Invoker>;
+
+    Button()
+    {
+        m_invoker = std::make_shared<Invoker>();
+    }
+
+    Button(SharedInvoker invoker) : m_invoker(invoker) {}
+
     ~Button() = default;
-    virtual void press() = 0;
+
+    virtual void press()
+    {
+        if (m_invoker != nullptr)
+            m_invoker->executeCommand();
+    };
+
+    SharedInvoker m_invoker;
+};
+
+using SharedButton = std::shared_ptr<Button>;
+
+// ---- CONCRETE CLASSES
+class OpenFileCommand : public Command
+{
+public:
+    OpenFileCommand() = default;
+    OpenFileCommand(std::string filename) : m_filename(filename) {};
+
+    ~OpenFileCommand() = default;
+
+    void execute() override
+    {
+        std::cout << "Opening file: " + m_filename + "..." << std::endl;
+    }
+
+private:
+    std::string m_filename;
 };
 
 int main()
 {
+    SharedButton openFileButton = std::make_shared<Button>();
+
+    Invoker::SharedCommand openLogsFile = std::make_shared<OpenFileCommand>("logs.txt");
+
+    openFileButton->m_invoker->setCommand(openLogsFile);
+    openFileButton->press();
 
     return 0;
 }
