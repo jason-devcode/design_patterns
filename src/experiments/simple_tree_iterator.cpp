c7e3cd180bde205f37948d971be817fd2c71deaa
jason-devcode <80931913+jason-devcode@users.noreply.github.com>
2025-03-02 22:07:18 -0500

    feat: add binary tree with iterator pattern experiment, pending culminate

---
+++ src/experiments/simple_tree_iterator.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+template <typename DataType>
+class BinaryTree
+{
+public: // inner classes
+    class TreeNode;
+    class Iterator;
+public: // interface
+    virtual void insertData() = 0;
+private:
+    std::shared_ptr<TreeNode> root = nullptr;
+};
+
+template <typename DataType>
+class BinaryTree<DataType>::TreeNode
+{
+public:
+    TreeNode() = default;
+    ~TreeNode() = default;
+
+    using SharedNode = std::shared_ptr<TreeNode>;
+
+    void setParentNode(SharedNode parentNode) { parent = parentNode; }
+    void insertLeftChildNode(SharedNode leftNode) { leftChild = leftNode; }
+    void insertRightChildNode(SharedNode rightNode) { rightChild = rightNode; }
+
+    SharedNode getLeftNode() const
+    {
+        return leftChild;
+    }
+
+    SharedNode getRightNode() const
+    {
+        return rightChild;
+    }
+
+    DataType getData() const
+    {
+        return m_data;
+    }
+
+private:
+    SharedNode parent = nullptr;
+    SharedNode leftChild = nullptr;
+    SharedNode rightChild = nullptr;
+
+private:
+    DataType m_data;
+};
+
+template <typename DataType>
+class BinaryTree<DataType>::Iterator
+{
+};
+
+using TreeFloats = BinaryTree<float>;
+
+int main()
+{
+    try
+    {
+        std::unique_ptr<TreeFloats> tree = std::make_unique<TreeFloats>();
+    }
+    catch (const std::exception &error)
+    {
+        std::cerr << error.what() << std::endl;
+    }
+    return 0;
+}
